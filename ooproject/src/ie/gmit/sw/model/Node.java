13b85cd32734755239f0d47e69028bfcda3d4e63
Joe <Joe@192.168.192.7>
2014-12-01 23:33:03 -0800

    test

---
+++ ooproject/src/ie/gmit/sw/model/Node.java
@@ -0,0 +1,18 @@
+package ie.gmit.sw.model;
+
+public class Node {
+	private String name;
+
+	public Node(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
